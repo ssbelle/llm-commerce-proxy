9f142d170c762bfc4973e724cada760a0b5d864b
Shawna <shawnalehman@gmail.com>
2026-01-28 12:22:18 -0500

    Update commerce.ts

--- commerce.ts
+++ commerce.ts
@@ -1,4 +1,4 @@
-// import type { VercelRequest, VercelResponse } from "@vercel/node";
+
 
 // TODO: replace with Foundry SDK client
 async function callFoundry(message: string) {
@@ -19,31 +19,6 @@ async function callFoundry(message: string) {
   };
 }
 
-// export default async function handler(
-//   req: VercelRequest,
-//   res: VercelResponse
-// ) {
-//   if (req.method !== "POST") {
-//     return res.status(405).json({ error: "Method not allowed" });
-//   }
-
-//   const { message } = req.body ?? {};
-
-//   if (!message) {
-//     return res.status(400).json({ error: "Missing message" });
-//   }
-
-//   console.log("COPILOT → VERCEL PROXY", message);
-
-//   try {
-//     const foundryResponse = await callFoundry(message);
-
-//     return res.status(200).json(foundryResponse);
-//   } catch (err: any) {
-//     console.error(err);
-//     return res.status(500).json({ error: "Foundry call failed" });
-//   }
-// }
 
 export default async function handler(req: any, res: any) {
   if (req.method !== "POST") {
